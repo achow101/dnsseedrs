c50fcfa0b349e3804f4914a783b5c01684edddef
Ava Chow <github@achow101.com>
2024-03-06 00:36:46 -0500

    Communicate with nodes

--- crawlrs/src/main.rs
+++ crawlrs/src/main.rs
@@ -1,8 +1,16 @@
 #![feature(ip)]
 
+use base32ct::{Base32Unpadded, Encoding};
+use bitcoin::consensus::{Decodable, Encodable};
+use bitcoin::p2p::address::{Address, AddrV2};
+use bitcoin::p2p::Magic;
+use bitcoin::p2p::message::{NetworkMessage, RawNetworkMessage};
+use bitcoin::p2p::message_network::VersionMessage;
+use bitcoin::p2p::ServiceFlags;
 use clap::Parser;
+use sha3::{Digest, Sha3_256};
 use std::io::{BufReader, BufWriter, Read, Write};
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpStream};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, Shutdown, SocketAddr, TcpStream};
 use std::str::FromStr;
 use std::time;
 use std::thread;
@@ -163,6 +171,98 @@ fn crawl_node(db_conn: &rusqlite::Connection, addr: String) {
             stream
         },
     };
+
+    let mut write_stream = BufWriter::new(&sock);
+    let mut read_stream = BufReader::new(&sock);
+
+    let net_magic = Magic::BITCOIN;
+
+    // Prep Version message
+    let addr_them = match &node_addr.host {
+        Host::Ipv4(ip) => Address{ services: ServiceFlags::NONE, address: ip.to_ipv6_mapped().segments(), port: node_addr.port },
+        Host::Ipv6(ip) => Address{ services: ServiceFlags::NONE, address: ip.segments(), port: node_addr.port },
+        Host::OnionV3(..) | Host::I2P(..) | Host::CJDNS(..) => Address{ services: ServiceFlags::NONE, address: [0, 0, 0, 0, 0, 0, 0, 0], port: node_addr.port },
+    };
+    let addr_me = Address{
+        services: ServiceFlags::NONE,
+        address: [0, 0, 0, 0, 0, 0, 0, 0],
+        port: 0,
+    };
+    let ver_msg = VersionMessage{
+        version: 70016,
+        services: ServiceFlags::NONE,
+        timestamp: i64::try_from(time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs()).unwrap(),
+        receiver: addr_them,
+        sender: addr_me,
+        nonce: 0,
+        user_agent: "/crawlrs:0.1.0/".to_string(),
+        start_height: -1,
+        relay: false,
+    };
+
+    // Send version message
+    RawNetworkMessage::new(net_magic, NetworkMessage::Version(ver_msg)).consensus_encode(&mut write_stream).unwrap();
+
+    // Send sendaddrv2 message
+    RawNetworkMessage::new(net_magic, NetworkMessage::SendAddrV2{}).consensus_encode(&mut write_stream).unwrap();
+    write_stream.flush().unwrap();
+
+    // Receive loop
+    loop {
+        let msg = RawNetworkMessage::consensus_decode(&mut read_stream).unwrap();
+        match msg.payload() {
+            NetworkMessage::Version(ver) => {
+                // Send verack
+                RawNetworkMessage::new(net_magic, NetworkMessage::Verack{}).consensus_encode(&mut write_stream).unwrap();
+
+                // Send getaddr
+                RawNetworkMessage::new(net_magic, NetworkMessage::GetAddr{}).consensus_encode(&mut write_stream).unwrap();
+            },
+            NetworkMessage::Addr(addrs) => {
+                for (_, a) in addrs {
+                    println!("Got addrv1 {}:{} with service flags {}", Ipv6Addr::from(a.address), a.port, a.services);
+                }
+                break
+            },
+            NetworkMessage::AddrV2(addrs) => {
+                for a in addrs {
+                    let addrstr = match a.addr {
+                        AddrV2::Ipv4(ip) => ip.to_string(),
+                        AddrV2::Ipv6(ip) => ip.to_string(),
+                        AddrV2::TorV2(host) => "who's advertising torv2????".to_string(),
+                        AddrV2::TorV3(host) => {
+                            let mut to_hash: Vec<u8> = vec![];
+                            to_hash.extend_from_slice(b".onion checksum");
+                            to_hash.extend_from_slice(&host);
+                            to_hash.push(0x03);
+                            let checksum = Sha3_256::new()
+                                .chain_update(to_hash)
+                                .finalize();
+
+                            let mut to_enc: Vec<u8> = vec![];
+                            to_enc.extend_from_slice(&host);
+                            to_enc.extend_from_slice(&checksum[0..2]);
+                            to_enc.push(0x03);
+
+                            (Base32Unpadded::encode_string(&to_enc) + ".onion").to_string()
+                        },
+                        AddrV2::I2p(host) => (Base32Unpadded::encode_string(&host) + ".b32.i2p").to_string(),
+                        AddrV2::Cjdns(ip) => ip.to_string(),
+                        _ => "unknown".to_string(),
+                    };
+                    println!("Got addrv2 {}:{} with service flags {}", addrstr, a.port, a.services);
+                }
+                break
+            },
+            NetworkMessage::Ping(ping) => {
+                RawNetworkMessage::new(net_magic, NetworkMessage::Pong(*ping)).consensus_encode(&mut write_stream).unwrap();
+            },
+            _ => (),
+        };
+        write_stream.flush().unwrap();
+    }
+
+    sock.shutdown(Shutdown::Both).unwrap();
 }
 
 fn main() {
